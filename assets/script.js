f7096cbefc35f5e90ea80eeb745959bab1d1e82d
Zak Monnet <zakmonnet@gmail.com>
2020-09-19 12:01:50 -0500

    added functionality for granim.js

--- assets/script.js
+++ assets/script.js
@@ -4,26 +4,42 @@ let song;
 const APIURLgetArtist = "https://theaudiodb.com/api/v1/json/1/search.php?s=";
 const APIURLgetAlbum = "https://theaudiodb.com/api/v1/json/1/searchalbum.php?a=";
 const APIURLgetSong = "https://theaudiodb.com/api/v1/json/1/search.php?s=";
+
 var granimInstance = new Granim({
-      element: '#canvas-complex',
+      element: '#canvas-basic',
       direction: 'left-right',
       isPausedWhenNotInView: true,
       states : {
           "default-state": {
               gradients: [
-                  [
-                      { color: '#833ab4', pos: .2 },
-                      { color: '#fd1d1d', pos: .8 },
-                      { color: '#38ef7d', pos: 1 }
-                  ], [
-                      { color: '#40e0d0', pos: 0 },
-                      { color: '#ff8c00', pos: .2 },
-                      { color: '#ff0080', pos: .75 }
-                  ],
+                  ['#ff9966', '#ff5e62'],
+                  ['#00F260', '#0575E6'],
+                  ['#e1eec3', '#f05053']
               ]
           }
       }
   });
+
+// var granimInstance = new Granim({
+//       element: '#canvas-complex',
+//       direction: 'left-right',
+//       isPausedWhenNotInView: true,
+//       states : {
+//           "default-state": {
+//               gradients: [
+//                   [
+//                       { color: '#833ab4', pos: .2 },
+//                       { color: '#fd1d1d', pos: .8 },
+//                       { color: '#38ef7d', pos: 1 }
+//                   ], [
+//                       { color: '#40e0d0', pos: 0 },
+//                       { color: '#ff8c00', pos: .2 },
+//                       { color: '#ff0080', pos: .75 }
+//                   ],
+//               ]
+//           }
+//       }
+//   });
 
 
 // var granimInstance = new Granim({
